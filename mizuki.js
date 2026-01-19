a20863294d8c27ef0bb291057341c617f2d86c7e
mizuki_nagasaki <mizukinagasaki300@gmail.com>
2026-01-20 08:28:10 +0900

    hedderの仮置き、ハンバーガー仮置き＋変形挙動、完了

--- mizuki.js
+++ mizuki.js
@@ -2,5 +2,6 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".header-nav");
 
 hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("is-open"); // ← 追加
     nav.classList.toggle("is-open");
 });
